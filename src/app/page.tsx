0c8ad9cb6856014df3f2cea11b43b3b533a4e191
sayan404 <sayanmajumder0002@gmail.com>
2024-06-27 14:50:51 +0530

    Phantom wallet connection & context setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main>Init</main>;
-}
+import Image from "next/image";
+// import Modal from "../components/Modal";
+import Link from "next/link";
+import MultiButton from "./components/Multibutton";
+
+const Index: React.FC = async () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center ">
+      Hey Solana is here
+      <MultiButton />
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/contexts/ContextProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { WalletError } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { FC, ReactNode, useCallback, useMemo } from "react";
+import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+export const WalletContextProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const wallets = useMemo(() => [], []);
+
+  const onError = useCallback((error: WalletError) => {
+    console.error(error);
+  }, []);
+
+  return (
+    <ConnectionProvider endpoint={process.env.NEXT_PUBLIC_HELIUS_URL!}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect={false}>
+        <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
