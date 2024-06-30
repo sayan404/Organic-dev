73f32c2dbe734d979272831d2b3e0e44e9ff3985
SOHAM DAS <sohamdas.nest@gmail.com>
2024-06-30 08:37:21 +0530

    added

--- src/app/hospital/donor-requests/page.tsx
+++ src/app/hospital/donor-requests/page.tsx
@@ -5,6 +5,55 @@ import { Divider } from "@nextui-org/react";
 import { useEffect } from "react";
 import axios from "axios";
 
+enum DonorTypes {
+  Live = "Live Donor",
+  Deceased = "Deceased Donor",
+}
+
+const DonorCardDetails = [
+  {
+    organ: "Heart",
+    type: DonorTypes.Live,
+    name: "John Doe",
+    Blood: "O+",
+    age: "25",
+    Address: "Kolkata, Pin-700001, WB"
+  },
+  {
+    organ: "Kidney",
+    type: DonorTypes.Deceased,
+    name: "Soham Das",
+    Blood: "O+",
+    age: "22",
+    Address: "Kolkata, Pin-700152, WB"
+  },
+  {
+    organ: "Liver",
+    type: DonorTypes.Live,
+    name: "Sayan Majumdar",
+    Blood: "B+",
+    age: "22",
+    Address: "Howrah, Pin-700152, WB"
+  },
+  {
+    organ: "Heart",
+    type: DonorTypes.Live,
+    name: "Animesh Bag",
+    Blood: "A+",
+    age: "27",
+    Address: "Medinipur, Pin-700152, WB"
+  },
+  {
+    organ: "Liver",
+    type: DonorTypes.Deceased,
+    name: "Alex",
+    Blood: "A+",
+    age: "30",
+    Address: "Medinipur, Pin-700152, WB"
+  },
+];
+
+
 const DonorRequestsPage = () => {
   // useEffect(() => {
   //   // document.title = "Donor Requests - ACME";
@@ -22,8 +71,8 @@ const DonorRequestsPage = () => {
       </div>
       <Divider />
       <div className="flex flex-wrap gap-4 mt-8">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <DonorRequestCard key={index} />
+        {DonorCardDetails.map((item, index) => (
+          <DonorRequestCard key={index} organ={item.organ} blood={item.Blood} type={item.type} name={item.name} age={item.age} address={item.Address}/>
         ))}
       </div>
     </div>
