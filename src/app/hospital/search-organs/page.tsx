51ae02cc48b7a96254fcf078247cd5a73f00ae64
SOHAM DAS <sohamdas.nest@gmail.com>
2024-06-30 10:12:54 +0530

    bug fix

--- src/app/hospital/search-organs/page.tsx
+++ src/app/hospital/search-organs/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { FunnelIcon } from '@heroicons/react/16/solid';
 import { Divider } from '@nextui-org/react';
 import { Button } from '@/components/ui/button';
 import { CardSection } from './CardSection';
+import { useRouter } from 'next/navigation';
 const SearchOrgans = () => {
+  const router = useRouter();
   return (
     <main>
       <div className='mx-[100px] my-[40px]'>
@@ -13,7 +14,7 @@ const SearchOrgans = () => {
         <Divider />
         {/* search btn */}
         <div className='flex justify-center my-4'>
-          <Button className='text-xl p-6'>Registration Form</Button>
+          <Button onClick={() => router.push("/hospital/patent-requests") } className='text-xl p-6'>Registration Form</Button>
         </div>
         <div className='flex justify-between'>
           <div className='text-4xl mb-8 font-semibold'>All Registrations</div>
